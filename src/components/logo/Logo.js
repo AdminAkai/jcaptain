cf0974f355f37cc96783d90cff3f04df1e1ac5fe
JCaptain <joshietrinidad@gmail.com>
2019-11-25 15:30:40 -0500

    Polygon mask Half Life Logo, glitch JCaptain Logo prep

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Logo.scss'
+
+export default class Logo extends Component {
+
+    render() {
+        return(
+            <div className="logo">
+                <div class="glitch" data-text="GLITCH">GLITCH</div> 
+            </div>
+        )
+    }
+} 
