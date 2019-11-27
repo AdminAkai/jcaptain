5cfc8e43276e1539721845c83c1980e067b57dc0
JCaptain <joshietrinidad@gmail.com>
2019-11-27 09:58:25 -0500

    Nuked dropdown styling

--- src/components/dropdown/DropDown.js
+++ src/components/dropdown/DropDown.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
-import './dropdown.scss'
+import './dropdown.css'
 
 export default class DropDown extends Component {
 
     render() {
         return(
-            <div>
-                <ul className="drop-menu menu-7">
-                    <li>Test</li>
-                    <li>Test2</li>
-                </ul>
+            <div className="container">
+                <div className="nav">
+                    <div className="dropdown">
+                        <ul>
+                            <li>
+                                <ul className="drop-menu menu-7">
+                                    <li>Test</li>
+                                    <li>Test2</li>
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
             </div>
+
         )
     }
 } 
