724bbad3c2249c2d131bdf3b974fa66e89cca057
JCaptain <joshietrinidad@gmail.com>
2019-12-11 16:37:20 -0500

    fixed links, added resume

--- src/components/logo/subcomponents/button/Button.js
+++ src/components/logo/subcomponents/button/Button.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react'
 import './button.scss'
 import '../../Logo.scss'
+import Resume from './resume.pdf'
 
 
 export default class Button extends Component {
 
+    state = {
+        aboutActivate: false,
+    }
+
+
+    activateAbout = (event) => {
+        event.preventDefault()
+        this.setState({aboutActivate: true})
+    }
+
+    deactivateAbout = (event) => {
+        event.preventDefault()
+        this.setState({aboutActivate: false})
+    }
 
     render() {
         return(
             <div className="frame animatedFadeInUp animated fadeInUp">
-                <div className="button">
-                <span>&lambda;BOUT</span>
-                    <svg>
-                    <polyline className="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
-                    <polyline className="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
-                </svg>
-                </div>
+                <a href={Resume} rel="noopener noreferrer" target="_blank">
+                    <div className="button">
+
+                    <span>&lambda;BOUT</span>
+                        <svg>
+                        <polyline className="o1" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                        <polyline className="o2" points="0 0, 150 0, 150 55, 0 55, 0 0"></polyline>
+                    </svg>
+                    </div>
+                </a>
                 <div className="projects">
                     <a href="https://jcaptainnnn.github.io/" rel="noopener noreferrer" target="_blank">
                         <div className="button-2">
@@ -45,6 +63,15 @@ export default class Button extends Component {
                         </div>
                     </a>
                 </div>
+            {this.state.aboutActivate
+            ?
+            <div className="about-container" onClick={this.deactivateAbout}>
+                <div className="about">
+                </div>
+            </div>
+            :
+            null
+            }
             </div>
         )
     }
